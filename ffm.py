4a15d0cf16d73cd011afe332b5c17c4c0e7b4790
alex <you@example.com>
2022-10-17 09:26:53 -0400

    maybe move to python?

---
+++ ffm.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+window = tk.TK()
+label = tk.Label(text="poop");
+label.pack()
+
+window.mainloop()
--- myprogram.java
+++ myprogram.java
@@ -1,26 +1,21 @@
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.plaf.LabelUI;
+import javax.swing.JButton;
 
 class myprogram{
     public static void main(String[] args){
         //Setting up the JFrame
         JFrame ApplicationWindow = new JFrame("Florida File Manager");
-        ApplicationWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //ApplicationWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ApplicationWindow.setSize(1280, 720);
         ApplicationWindow.setResizable(false);
         ApplicationWindow.setLayout(null);
         ApplicationWindow.setVisible(true);
 
         JButton booty = new JButton("AHHHH");
-        booty.setLocation(30, 30);
-        booty.setSize(400,400);
-        booty.setVisible(true);
+        //booty.setLocation(30, 30);
+        //booty.setSize(400,400);
+        booty.setBounds(100, 100, 100, 50);
+
         ApplicationWindow.add(booty);
-
-
-        //LabelUI label = new LabelUI("AHHHHHH");
-        //label.add
-
     }
 }
